b87f057e60ba48d8e3f8c8abd4da277afce26ab7
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2024-08-30 22:54:40 +0800

    testesss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -275,7 +275,7 @@
         </video>
         <div class="content">
             <img src="{{ asset('storage/logo2.png') }}" width="150px" class="mb-5" alt="Logo Konawe">
-            <h1 class="masthead-heading text-uppercase mb-0">Selamat Datang kontol</h1>
+            <h1 class="masthead-heading text-uppercase mb-0">Selamat Datang</h1>
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
                 <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
